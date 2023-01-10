3b6d824ee78b24a1b84655689df94877f1b59008
shinchangyoung <gojj294@gmail.com>
2023-01-10 16:32:25 +0900

    My-Todo start

---
+++ src/componets/Template.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Template = () => {
+    return (
+        <div>
+            <div>오늘의 할일 (0)</div>
+            <div>할 일 목록이 들어올자 은선님 고마워여</div>
+        </div>
+    );
+};
+
+export default Template;
